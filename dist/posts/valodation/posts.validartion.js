f2baddd1dc51b9e5601a7e8ac3ddc71b61f6c2a3
orisborlov <mrwiggle40000@gmail.com>
2023-08-18 13:37:17 +0700

    fixed: error messages for not existing params

--- dist/posts/valodation/posts.validartion.js
+++ dist/posts/valodation/posts.validartion.js
@@ -3,13 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.authBlogIsExistsValidationMiddleware = exports.postBlogIdValidation = exports.postContenteValidation = exports.postShortDescriptionValidation = exports.postTitleValidation = void 0;
 const express_validator_1 = require("express-validator");
 const blogs_db_1 = require("../../blogs/db/blogs.db");
-const postTitleValidation = () => (0, express_validator_1.body)('title').isString().trim().isLength({ min: 1, max: 30 }).withMessage({ message: 'Invalid title', field: "title" });
+const postTitleValidation = () => (0, express_validator_1.body)('title').exists({ values: "falsy" }).withMessage({ message: 'title not passed', field: "title" }).isString().trim().isLength({ min: 1, max: 30 }).withMessage({ message: 'Invalid title', field: "title" });
 exports.postTitleValidation = postTitleValidation;
-const postShortDescriptionValidation = () => (0, express_validator_1.body)('shortDescription').isString().trim().isLength({ min: 1, max: 100 }).withMessage({ message: 'Invalid shortDescription', field: "shortDescription" });
+const postShortDescriptionValidation = () => (0, express_validator_1.body)('shortDescription').exists({ values: "falsy" }).withMessage({ message: 'shortDescription not passed', field: "shortDescription" }).isString().trim().isLength({ min: 1, max: 100 }).withMessage({ message: 'Invalid shortDescription', field: "shortDescription" });
 exports.postShortDescriptionValidation = postShortDescriptionValidation;
-const postContenteValidation = () => (0, express_validator_1.body)('content').isString().trim().isLength({ min: 1, max: 1000 }).withMessage({ message: 'Invalid content', field: "content" });
+const postContenteValidation = () => (0, express_validator_1.body)('content').exists({ values: "falsy" }).withMessage({ message: 'Content not passed', field: "content" }).isString().trim().isLength({ min: 1, max: 1000 }).withMessage({ message: 'Invalid content', field: "content" });
 exports.postContenteValidation = postContenteValidation;
-const postBlogIdValidation = () => (0, express_validator_1.body)('blogId').isString().withMessage({ message: 'Invalid blogId', field: "blogId" });
+const postBlogIdValidation = () => (0, express_validator_1.body)('blogId').exists({ values: "falsy" }).withMessage({ message: 'blogId not passed', field: "blogId" }).isString().withMessage({ message: 'Invalid blogId', field: "blogId" });
 exports.postBlogIdValidation = postBlogIdValidation;
 const authBlogIsExistsValidationMiddleware = () => (0, express_validator_1.body)('blogId').custom((value, { req }) => {
     const blogToFetch = blogs_db_1.blogsDB.find(blog => blog.id === req.body.blogId);
--- src/posts/valodation/posts.validartion.ts
+++ src/posts/valodation/posts.validartion.ts
@@ -1,10 +1,10 @@
 import { body } from "express-validator"
 import { blogsDB } from "../../blogs/db/blogs.db"
 
-export const postTitleValidation = () => body('title').isString().trim().isLength({min:1, max:30}).withMessage({message: 'Invalid title', field:"title"})
-export const postShortDescriptionValidation = () => body('shortDescription').isString().trim().isLength({min:1, max:100}).withMessage({message: 'Invalid shortDescription', field:"shortDescription"})
-export const postContenteValidation = () => body('content').isString().trim().isLength({min:1, max:1000}).withMessage({message: 'Invalid content', field:"content"})
-export const postBlogIdValidation = () => body('blogId').isString().withMessage({message: 'Invalid blogId', field:"blogId"})
+export const postTitleValidation = () => body('title').exists({values:"falsy"}).withMessage({message: 'title not passed', field:"title"}).isString().trim().isLength({min:1, max:30}).withMessage({message: 'Invalid title', field:"title"})
+export const postShortDescriptionValidation = () => body('shortDescription').exists({values:"falsy"}).withMessage({message: 'shortDescription not passed', field:"shortDescription"}).isString().trim().isLength({min:1, max:100}).withMessage({message: 'Invalid shortDescription', field:"shortDescription"})
+export const postContenteValidation = () => body('content').exists({values:"falsy"}).withMessage({message: 'Content not passed', field:"content"}).isString().trim().isLength({min:1, max:1000}).withMessage({message: 'Invalid content', field:"content"})
+export const postBlogIdValidation = () => body('blogId').exists({values:"falsy"}).withMessage({message: 'blogId not passed', field:"blogId"}).isString().withMessage({message: 'Invalid blogId', field:"blogId"})
 
 
 
