c62b1382d9b210396b599ff44be0203463cc00f4
orisborlov <mrwiggle40000@gmail.com>
2023-08-18 14:11:04 +0700

    fixed: check existance of fields in blogs

--- dist/blogs/validation/blog.validatiom.js
+++ dist/blogs/validation/blog.validatiom.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.blogUrlMatchingValidation = exports.blogUrlValidation = exports.blogDescriptionValidation = exports.blogNameValidation = void 0;
 const express_validator_1 = require("express-validator");
-const blogNameValidation = () => (0, express_validator_1.body)('name').isString().trim().isLength({ min: 1, max: 15 }).withMessage({ message: 'Invalid name', field: "name" });
+const blogNameValidation = () => (0, express_validator_1.body)('name').exists({ values: "falsy" }).withMessage({ message: 'name not passed', field: "name" }).isString().trim().isLength({ min: 1, max: 15 }).withMessage({ message: 'Invalid name', field: "name" });
 exports.blogNameValidation = blogNameValidation;
-const blogDescriptionValidation = () => (0, express_validator_1.body)('description').isString().trim().isLength({ min: 1, max: 500 }).withMessage({ message: 'Invalid description', field: "description" });
+const blogDescriptionValidation = () => (0, express_validator_1.body)('description').exists({ values: "falsy" }).withMessage({ message: 'description not passed', field: "description" }).isString().trim().isLength({ min: 1, max: 500 }).withMessage({ message: 'Invalid description', field: "description" });
 exports.blogDescriptionValidation = blogDescriptionValidation;
-const blogUrlValidation = () => (0, express_validator_1.body)('websiteUrl').isString().trim().isLength({ min: 1, max: 100 }).withMessage({ message: 'Invalid websiteUrl', field: "websiteUrl" });
+const blogUrlValidation = () => (0, express_validator_1.body)('websiteUrl').exists({ values: "falsy" }).withMessage({ message: 'websiteUrl not passed', field: "websiteUrl" }).isString().trim().isLength({ min: 1, max: 100 }).withMessage({ message: 'Invalid websiteUrl', field: "websiteUrl" });
 exports.blogUrlValidation = blogUrlValidation;
 const blogUrlMatchingValidation = () => (0, express_validator_1.body)('websiteUrl').matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage({ message: 'Invalid websiteUrl', field: "websiteUrl" });
 exports.blogUrlMatchingValidation = blogUrlMatchingValidation;
--- src/blogs/validation/blog.validatiom.ts
+++ src/blogs/validation/blog.validatiom.ts
@@ -1,9 +1,9 @@
 import { body } from "express-validator"
 
-export const blogNameValidation = () => body('name').isString().trim().isLength({min:1,max:15}).withMessage({message: 'Invalid name', field:"name"})
+export const blogNameValidation = () => body('name').exists({values:"falsy"}).withMessage({message: 'name not passed', field:"name"}).isString().trim().isLength({min:1,max:15}).withMessage({message: 'Invalid name', field:"name"})
 
-export const blogDescriptionValidation = () => body('description').isString().trim().isLength({min:1,max:500}).withMessage({message: 'Invalid description', field:"description"})
+export const blogDescriptionValidation = () => body('description').exists({values:"falsy"}).withMessage({message: 'description not passed', field:"description"}).isString().trim().isLength({min:1,max:500}).withMessage({message: 'Invalid description', field:"description"})
 
-export const blogUrlValidation = () => body('websiteUrl').isString().trim().isLength({min:1,max:100}).withMessage({message: 'Invalid websiteUrl', field:"websiteUrl"})
+export const blogUrlValidation = () => body('websiteUrl').exists({values:"falsy"}).withMessage({message: 'websiteUrl not passed', field:"websiteUrl"}).isString().trim().isLength({min:1,max:100}).withMessage({message: 'Invalid websiteUrl', field:"websiteUrl"})
 
 export const blogUrlMatchingValidation = () => body('websiteUrl').matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage({message: 'Invalid websiteUrl', field:"websiteUrl"})
