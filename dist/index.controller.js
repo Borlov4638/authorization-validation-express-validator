e289164d637955bee0f638d44db076f2082301c3
orisborlov <mrwiggle40000@gmail.com>
2023-08-21 16:44:28 +0700

    fixed: replaced endpoint for deleting data

--- dist/index.controller.js
+++ dist/index.controller.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,15 +15,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const blogs_router_1 = require("./blogs/blogs.roter/blogs.router");
-const blogs_db_1 = require("./blogs/db/blogs.db");
 const posts_router_1 = require("./posts/posts.router/posts.router");
-const posts_db_1 = require("./posts/db/posts.db");
+const db_init_1 = require("./blogs/db/db.init");
 exports.app = (0, express_1.default)();
 exports.app.use(express_1.default.json());
 exports.app.use('/blogs', blogs_router_1.blogsRouter);
 exports.app.use('/posts', posts_router_1.postRouter);
-exports.app.delete('/testing/all-data', (req, res) => {
-    blogs_db_1.blogsDB.splice(0, blogs_db_1.blogsDB.length);
-    posts_db_1.postsDB.splice(0, posts_db_1.postsDB.length);
+exports.app.delete('/testing/all-data', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield db_init_1.client.db("incubator").collection("blogs").deleteMany({});
+    yield db_init_1.client.db("incubator").collection("posts").deleteMany({});
     res.sendStatus(204);
-});
+}));
--- src/index.controller.ts
+++ src/index.controller.ts
@@ -3,6 +3,8 @@ import { blogsRouter } from "./blogs/blogs.roter/blogs.router"
 import { blogsDB } from "./blogs/db/blogs.db"
 import { postRouter } from "./posts/posts.router/posts.router"
 import { postsDB } from "./posts/db/posts.db"
+import { ClientRequest } from "http"
+import { client } from "./blogs/db/db.init"
 
 export const app = express()
 
@@ -12,8 +14,8 @@ app.use('/blogs',blogsRouter)
 
 app.use('/posts', postRouter)
 
-app.delete('/testing/all-data', (req:Request, res:Response) =>{
-    blogsDB.splice(0, blogsDB.length)
-    postsDB.splice(0, postsDB.length)
+app.delete('/testing/all-data', async (req:Request, res:Response) =>{
+    await client.db("incubator").collection("blogs").deleteMany({})
+    await client.db("incubator").collection("posts").deleteMany({})
     res.sendStatus(204)
 })
