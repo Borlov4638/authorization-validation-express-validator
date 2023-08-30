4dfdef2b8787cb55fc2a7f9d6242921544dcb3df
orisborlov <mrwiggle40000@gmail.com>
2023-08-30 17:12:31 +0700

    added delete and get endpoints for comments

--- dist/comments/comments.router.js
+++ dist/comments/comments.router.js
@@ -29,11 +29,34 @@ exports.commentsRouter.put('/:commentId', (0, comments_validation_1.commentsCont
     if (!commentToUpdate) {
         return res.sendStatus(404);
     }
-    console.log(isAuthorized.userId);
-    console.log(commentToUpdate.commentatorInfo.userId);
     if (isAuthorized.userId !== commentToUpdate.commentatorInfo.userId) {
         return res.sendStatus(403);
     }
     yield db_init_1.client.db('incubator').collection('comments').updateOne(commentToUpdate, { $set: { content: req.body.content } });
+    return res.sendStatus(204);
+}));
+exports.commentsRouter.get('/:commentId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const commentToShow = yield db_init_1.client.db('incubator').collection('comments').findOne({ _id: new mongodb_1.ObjectId(req.params.commentId) }, { projection: { _id: 0, postId: 0 } });
+    if (!commentToShow) {
+        return res.sendStatus(404);
+    }
+    return res.status(200).send(commentToShow);
+}));
+exports.commentsRouter.delete('/:commentId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!req.headers.authorization) {
+        return res.sendStatus(401);
+    }
+    const isAuthorized = jwt_service_1.jwtService.getUserByToken(req.headers.authorization);
+    if (!isAuthorized) {
+        return res.sendStatus(401);
+    }
+    const commentToDelete = yield db_init_1.client.db('incubator').collection('comments').findOne({ _id: new mongodb_1.ObjectId(req.params.commentId) });
+    if (!commentToDelete) {
+        return res.sendStatus(404);
+    }
+    if (isAuthorized.userId !== commentToDelete.commentatorInfo.userId) {
+        return res.sendStatus(403);
+    }
+    yield db_init_1.client.db('incubator').collection('comments').deleteOne(commentToDelete);
     return res.sendStatus(204);
 }));
--- src/comments/comments.router.ts
+++ src/comments/comments.router.ts
@@ -30,9 +30,6 @@ commentsRouter.put('/:commentId',
             return res.sendStatus(404)
         }
 
-        console.log(isAuthorized.userId)
-        console.log(commentToUpdate.commentatorInfo.userId)
-
         if(isAuthorized.userId !== commentToUpdate.commentatorInfo.userId){
             return res.sendStatus(403)
         }
@@ -43,3 +40,43 @@ commentsRouter.put('/:commentId',
 
 })
 
+commentsRouter.get('/:commentId',
+    async (req:RequestWithParam<{commentId:string}>, res:Response) =>{
+        
+        const commentToShow = await client.db('incubator').collection('comments').findOne({_id: new ObjectId(req.params.commentId)}, {projection:{_id:0, postId:0}})
+
+        if(!commentToShow){
+            return res.sendStatus(404)
+        }
+
+        return res.status(200).send(commentToShow)
+})
+
+
+
+commentsRouter.delete('/:commentId',
+    async (req:RequestWithParam<{commentId:string}>, res:Response) =>{
+        
+        if(!req.headers.authorization){
+            return res.sendStatus(401)
+        }
+
+        const isAuthorized = jwtService.getUserByToken(req.headers.authorization) as jwtUser
+
+        if(!isAuthorized){
+            return res.sendStatus(401)
+        }
+
+        const commentToDelete = await client.db('incubator').collection('comments').findOne({_id: new ObjectId(req.params.commentId)})
+
+        if(!commentToDelete){
+            return res.sendStatus(404)
+        }
+
+        if(isAuthorized.userId !== commentToDelete.commentatorInfo.userId){
+            return res.sendStatus(403)
+        }
+
+        await client.db('incubator').collection('comments').deleteOne(commentToDelete)
+        return res.sendStatus(204)
+})
