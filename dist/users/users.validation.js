946f9c743c0026200237c68cd40ce62586deb696
orisborlov <mrwiggle40000@gmail.com>
2023-09-01 12:34:58 +0700

    fix: check on login unique

--- dist/users/users.validation.js
+++ dist/users/users.validation.js
@@ -12,7 +12,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.usersEmailValidation = exports.usersPasswordValidation = exports.usersLoginValidation = void 0;
 const express_validator_1 = require("express-validator");
 const db_init_1 = require("../blogs/db/db.init");
-const usersLoginValidation = () => (0, express_validator_1.body)('login').exists().withMessage({ message: "login is not passed", field: "login" }).isString().isLength({ min: 3, max: 10 }).withMessage({ message: "invalid length of login", field: "login" }).matches(/^[a-zA-Z0-9_-]*$/).withMessage({ message: "invalid login", field: "login" });
+const usersLoginValidation = () => (0, express_validator_1.body)('login').exists().withMessage({ message: "login is not passed", field: "login" }).isString().isLength({ min: 3, max: 10 }).withMessage({ message: "invalid length of login", field: "login" }).matches(/^[a-zA-Z0-9_-]*$/).withMessage({ message: "invalid login", field: "login" })
+    .custom((value, { req }) => __awaiter(void 0, void 0, void 0, function* () {
+    const isLoginExists = yield db_init_1.client.db('incubator').collection('users').find({ login: req.body.login }).toArray();
+    if (isLoginExists.length > 0) {
+        throw new Error();
+    }
+    return true;
+})).withMessage({ message: 'This login is alreaidy in use', field: "login" });
 exports.usersLoginValidation = usersLoginValidation;
 const usersPasswordValidation = () => (0, express_validator_1.body)('password').exists().withMessage({ message: "password is not passed", field: "password" }).isString().isLength({ min: 6, max: 20 }).withMessage({ message: "invalid password", field: "password" });
 exports.usersPasswordValidation = usersPasswordValidation;
