81e93c41ce1069f2b3b4002957d580695e8c3986
orisborlov <mrwiggle40000@gmail.com>
2023-09-13 18:18:42 +0700

    fix:mongoose dep

--- dist/auth/auth.validation.js
+++ dist/auth/auth.validation.js
@@ -7,7 +7,7 @@ const authPasswordValidation = () => (0, express_validator_1.body)('password').e
 exports.authPasswordValidation = authPasswordValidation;
 const authLoginOrEmailValidation = () => (0, express_validator_1.body)('loginOrEmail').exists().withMessage({ message: "login or email is not passed", field: "loginOrEmail" }).isString().isLength({ min: 1 }).withMessage({ message: "login or email is invalid", field: "loginOrEmail" });
 exports.authLoginOrEmailValidation = authLoginOrEmailValidation;
-const authNewPasswordValidation = () => (0, express_validator_1.body)('newPassword').exists().withMessage({ message: "password is not passed", field: "password" }).isString().isLength({ min: 1 }).withMessage({ message: "password is invalid", field: "password" });
+const authNewPasswordValidation = () => (0, express_validator_1.body)('newPassword').exists().withMessage({ message: "password is not passed", field: "password" }).isString().isLength({ min: 6, max: 20 }).withMessage({ message: "password is invalid", field: "password" });
 exports.authNewPasswordValidation = authNewPasswordValidation;
 const authRecoveryCodeValidation = () => (0, express_validator_1.body)('recoveryCode').exists().withMessage({ message: "recoveryCode is not passed", field: "recoveryCode" }).isString().isLength({ min: 1 }).withMessage({ message: "code must be a string", field: "recoveryCode" })
     .custom((value, { req }) => {
